9256b5e2520dd162df4ffbc3e877849837077a94
svyatoslav.d <svyatoslav.d@blaize.tech>
2020-09-18 15:10:20 +0300

    protocol version

--- src/version.h
+++ src/version.h
@@ -11,20 +11,20 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70919;
+static const int PROTOCOL_VERSION = 71701;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 70077;
+static const int GETHEADERS_VERSION = 71110;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 70918;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 70919;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71701;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71701;
 
 //! masternodes older than this proto version use old strMessage format for mnannounce
-static const int MIN_PEER_MNANNOUNCE = 70913;
+static const int MIN_PEER_MNANNOUNCE = 71701;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
