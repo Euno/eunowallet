1923e83b93add1ab58a8c5cff8e9c94412f5a310
svyatoslav.d <svyatoslav.d@blaize.tech>
2020-10-07 09:55:52 +0300

    set max block size limit to 8 MiB

--- src/policy/policy.h
+++ src/policy/policy.h
@@ -16,7 +16,7 @@ class CChainParams;
 class CCoinsViewCache;
 
 /** Default for -blockmaxsize and -blockminsize, which control the range of sizes the mining code will create **/
-static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 750000;
+static const unsigned int DEFAULT_BLOCK_MAX_SIZE = 8388608;
 static const unsigned int DEFAULT_BLOCK_MIN_SIZE = 0;
 /** Default for -blockprioritysize, maximum space for zero/low-fee transactions **/
 static const unsigned int DEFAULT_BLOCK_PRIORITY_SIZE = 50000;
